b8b636f686c3e50b3d518e275028fbf6642da327
Tushar Sanchaniya <tushar.sanchaniya@mindinventory.com>
2022-11-16 14:03:26 +0530

    feat(neopop): fix ui issue of floating button clickable area

--- src/components/floatingButton/useFloatingButton.ts
+++ src/components/floatingButton/useFloatingButton.ts
@@ -4,8 +4,8 @@ import type { FloatingButtonProps } from './floatingButton.type';
 
 export const useFloatingButton = (props: FloatingButtonProps) => {
   const {
-    backgroundColor = 'rgba(250, 226, 46,1)',
-    bottomShadowColor = 'rgba(0, 0, 0,1)',
+    backgroundColor = 'rgba(250, 226, 46, 1)',
+    bottomShadowColor = 'rgba(0, 0, 0, 1)',
     depth = 15,
     height: propHeight = 200,
     isFloating = true,
