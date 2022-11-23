e676e14c8d9105c3bae4e5fe9a4c4c96a43df522
Premshankar Shah <premshankar.shah@mindinventory.com>
2022-11-23 17:39:55 +0530

    feat(card): animation in card borders added

--- src/components/card/backgroundGradient.tsx
+++ src/components/card/backgroundGradient.tsx
@@ -5,6 +5,8 @@ import {
   Canvas,
   RoundedRect,
   SweepGradient,
+  useComputedValue,
+  useTiming,
   vec,
 } from '@shopify/react-native-skia';
 
@@ -20,9 +22,26 @@ interface BackgroundGradientProps {
 const BackgroundGradient = (props: BackgroundGradientProps) => {
   const { blur, borderColors, canvasPadding, cardRadius, height, width } =
     props;
+
+  const progress = useTiming({
+    from: 0,
+    loop: true,
+    to: 6,
+    // yoyo: true,
+  });
+
+  const transform = useComputedValue(
+    () => [{ rotate: progress.current }],
+    [progress]
+  );
+
   return (
     <Canvas
-      style={{ height: height + canvasPadding, width: width + canvasPadding }} children={(<>
+      style={{
+        height: height + canvasPadding,
+        width: width + canvasPadding,
+      }}
+      children={
         <RoundedRect
           x={canvasPadding / 2}
           y={canvasPadding / 2}
@@ -32,11 +51,20 @@ const BackgroundGradient = (props: BackgroundGradientProps) => {
           r={cardRadius}
         >
           <SweepGradient
+            origin={vec(
+              (width + canvasPadding) / 2,
+              (height + canvasPadding) / 2
+            )}
             c={vec((width + canvasPadding) / 2, (height + canvasPadding) / 2)}
             colors={borderColors}
+            transform={transform}
           />
-          <BlurMask blur={blur} style="solid" />
-        </RoundedRect></>)} accessibilityLabelledBy={undefined} accessibilityLanguage={undefined} />
+          <BlurMask blur={blur} style={'solid'} />
+        </RoundedRect>
+      }
+      accessibilityLabelledBy={undefined}
+      accessibilityLanguage={undefined}
+    />
   );
 };
 
