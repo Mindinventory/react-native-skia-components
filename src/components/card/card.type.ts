96acf8230436c832993a431d3d651ae4e91f632d
Premshankar Shah <premshankar.shah@mindinventory.com>
2022-11-30 16:10:56 +0530

    feat(card): card component props definition provided

--- src/components/card/card.type.ts
+++ src/components/card/card.type.ts
@@ -1,26 +1,65 @@
 import type { ViewStyle } from 'react-native';
 
 export interface CardProps {
-  /*
-  badfkdkn ex
-  */
+  /**
+   * Defaults to black.
+   */
   backgroundColor?: string;
+  /**
+   * Blur mask of border of the card.
+   * Defaults to 10.
+   */
   blur?: number;
-  /*
-  badfkdkn exampe ['grey'. '#fgfff', 'reg.()]
-  */
+  /**
+   * Provide an borderColors in array format.
+   * For example ['red'] , ['green', 'black']
+   * Defaults to ['cyan', 'magenta', 'yellow', 'cyan'].
+   */
   borderColors?: string[];
+  /**
+   * Defaults to 5.
+   */
   borderWidth?: number;
+  /**
+   * Defaults to 20.
+   */
   cardRadius?: number;
+  /**
+   * render elements render inside the card.
+   */
   children: JSX.Element;
+  /**
+   * Defaults to 256.
+   * You can provide number
+   */
   height?: number;
+  /**
+   * Defaults to screen size width of 90%.
+   * Example ScreenWidth * 0.9.
+   * You can provide number.
+   */
   width?: number;
+  /**
+   * Animate inside card children elements to none, rotate and bounce.
+   */
   animation?: AnimationTypes;
+  /**
+   * Provide an style to inner elements of the card.
+   */
   style?: ViewStyle;
+  /**
+   * Animate Border colors to the none, normal, disco, yoyo wise.
+   */
   animateBorder?: AnimateBorderTypes;
+  /**
+   * Defaults to 3000.
+   */
   duration?: number;
 }
 
+/**
+ * BackgroundGradientProps props for canvas elements
+ */
 export interface BackgroundGradientProps {
   blur: number;
   borderColors: string[];
